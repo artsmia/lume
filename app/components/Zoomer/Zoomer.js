2e144343acb626807eeaeddb413e4db0b2028b26
Carl Peaslee <cpeaslee@gmail.com>
2017-09-14 20:20:25 -0500

    cropper is mostly working now

--- app/components/Zoomer/Zoomer.js
+++ app/components/Zoomer/Zoomer.js
@@ -1,7 +1,7 @@
 import {Component} from 'react'
 import styled from 'styled-components'
 import LeafletCss from './LeafletCss'
-import {s3Url, apiUrl} from '../../config'
+import {s3Url, apiUrl, url} from '../../config'
 const L = (typeof window === 'object') ? require('leaflet') : null
 
 
@@ -9,7 +9,10 @@ export default class extends Component {
 
   state = {
     zoomCreated: false,
-    zoomLoading: false
+    zoomLoading: false,
+    topleft: false,
+    bottomright: false,
+    cropping: false
   }
 
   render() {
@@ -31,6 +34,184 @@ export default class extends Component {
     } = this.state
     if (!zoomLoading && !zoomLoaded && !this.props.data.loading) {
       this.createZoomer()
+    }
+  }
+
+  createCropper = async () => {
+    try {
+
+      const {
+        topleft,
+        bottomright,
+      } = this.state
+
+
+      L.Control.Cropper = L.Control.extend({
+
+
+        onAdd(map){
+
+          map.cropStart = false
+          map.cropEnd = false
+          map.cropping = false
+          map.highlight = false
+
+          this.button = L.DomUtil.create('div')
+          L.DomUtil.setClass(this.button, "crop-button")
+
+          L.DomEvent.on(
+            this.button,
+            "click",
+            (e)=>{
+              if (!map.cropping && !map.cropStart && !map.cropEnd) {
+                map.cropping = true
+              } else if (!map.cropping && map.cropStart && map.cropEnd) {
+                map.cropping = true
+                map.cropStart = false
+                map.cropEnd = false
+              }
+            }
+          )
+
+          return this.button
+        },
+
+      })
+      L.control.cropper = function(...args){
+        return new L.Control.Cropper(...args)
+      }
+
+      L.control.cropper({
+        position: "bottomleft"
+      }).addTo(this.map)
+
+      let icon = L.icon({
+        iconUrl: `${url}/static/x.png`,
+        iconSize: 30,
+        iconAnchor: [15,15]
+      })
+
+      function createMarker(latlng){
+        return L.marker(latlng, {
+          icon,
+          draggable: true
+        })
+      }
+
+      const highlightSelection = () =>{
+
+        let {_northEast, _southWest} = this.map.getBounds()
+        let north = _northEast.lat
+        let south = _southWest.lat
+        let east = _northEast.lng
+        let west = _southWest.lng
+        let lats = [
+          this.map.cropStart._latlng.lat,
+          this.map.cropEnd._latlng.lat
+        ]
+        let lngs = [
+          this.map.cropStart._latlng.lng,
+          this.map.cropEnd._latlng.lng
+        ]
+        let top = Math.max(...lats)
+        let bottom = Math.min(...lats)
+        let left = Math.min(...lngs)
+        let right = Math.max(...lngs)
+
+
+        let bounds = [
+          [
+            [north, west],
+            [north, east],
+            [south, east],
+            [south, west]],
+          [
+            [top, left],
+            [top, right],
+            [bottom, right],
+            [bottom, left]
+          ]
+        ]
+
+        if (this.map.highlight) {
+          this.map.highlight.setLatLngs(bounds)
+        } else {
+          this.map.highlight = L.polygon(bounds, {
+            stroke: false,
+            fillColor: "black",
+            fillOpacity: .5,
+          })
+          this.map.highlight.addTo(this.map)
+        }
+
+      }
+
+
+
+      this.map.on(
+        "mousedown",
+        ({latlng})=>{
+          if (!this.map.cropStart && this.map.cropping) {
+            this.map.cropStart = createMarker(latlng)
+            this.map.cropStart.on("moveend", ({latlng}) => {
+              highlightSelection()
+              this.setState({
+                cropStart: this.map.cropStart,
+                cropEnd: this.map.cropEnd
+              })
+            })
+            this.map.cropStart.on("drag", ({latlng}) => {
+              highlightSelection()
+            })
+            this.map.cropStart.addTo(this.map)
+          }
+        }
+      )
+
+      this.map.on(
+        "mousemove",
+        ({latlng}) => {
+
+          if (this.map.cropping && this.map.cropStart && this.map.cropEnd){
+            this.map.cropEnd.setLatLng(latlng)
+            this.map.cropEnd.setLatLng(latlng)
+            highlightSelection()
+          }else if (this.map.cropping && this.map.cropStart && !this.map.cropEnd) {
+            this.map.cropEnd = createMarker(latlng)
+
+            this.map.cropEnd.addTo(this.map)
+            this.map.cropEnd.on("moveend", ({latlng}) => {
+              highlightSelection()
+              this.setState({
+                cropStart: this.map.cropStart,
+                cropEnd: this.map.cropEnd
+              })
+            })
+            this.map.cropEnd.on("drag", ({latlng}) => {
+              highlightSelection()
+            })
+          }
+        }
+      )
+
+      this.map.on(
+        "mouseup",
+        ({latlng}) => {
+
+          if (this.map.cropEnd && this.map.cropping) {
+            this.map.cropEnd.setLatLng(latlng)
+            this.setState({
+              cropStart: this.map.cropStart,
+              cropEnd: this.map.cropEnd
+            })
+            this.map.cropping = false
+            highlightSelection()
+          }
+        }
+      )
+
+    } catch (ex) {
+      console.error(ex)
     }
   }
 
@@ -97,7 +278,8 @@ export default class extends Component {
       this.map = L.map(mapRef, {
         crs: L.CRS.Simple,
         maxBounds: bounds,
-        zoomSnap: 0
+        zoomSnap: 0,
+        dragging: false
       })
 
       const container = this.map.getContainer()
@@ -116,8 +298,6 @@ export default class extends Component {
           image.src = this._url.replace("{z}",z).replace("{x}", x).replace("{y}", y)
           image.style["object-fit"] = "contain"
           tile.appendChild(image)
-          // tile.style.outline = '1px solid red'
-
           return tile
         }
       })
@@ -127,14 +307,17 @@ export default class extends Component {
       }
 
 
-      this.tiles = L.tileLayer.knight(`${s3Url}/${bucketId}/${imageId}/tiles/{z}-{x}-{y}.png`, {
-        tileSize,
-        maxNativeZoom: maxZoom,
-        minNativeZoom: 0,
-        noWrap: true,
-        bounds,
-        minZoom: initialZoom
-      })
+      this.tiles = L.tileLayer.knight(
+        `${s3Url}/${bucketId}/${imageId}/tiles/{z}-{x}-{y}.png`, {
+          tileSize,
+          maxNativeZoom: maxZoom,
+          minNativeZoom: 0,
+          noWrap: true,
+          bounds,
+          minZoom: initialZoom,
+          maxZoom
+        }
+      )
 
 
       this.map.setView([height / 2, -1 * width / 2], initialZoom)
@@ -146,7 +329,7 @@ export default class extends Component {
         if (zoomReq < initialZoom) {
           e.target._zoom = initialZoom
         } else {
-          e.target._zoom = Math.round(zoomReq * 2) / 2
+          e.target._zoom = Math.round(zoomReq)
         }
 
       })
@@ -157,6 +340,7 @@ export default class extends Component {
       this.map.invalidateSize()
       this.map.fitBounds(bounds)
 
+      this.createCropper()
 
       await this.promiseState( (prevState) => {
         return {
@@ -184,4 +368,14 @@ export default class extends Component {
 const ZoomerMap = styled.div`
   height: 100%;
   width: 100%;
+  .crop-button {
+    height: 50px;
+    width: 50px;
+    background-color: purple;
+  }
+  .crop-button-clicked {
+    height: 50px;
+    width: 50px;
+    background-color: yellow;
+  }
 `
