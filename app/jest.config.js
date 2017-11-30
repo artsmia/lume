dc9512878bd0b4a39acf89309096c90fcfecae08
Carl Peaslee <cpeaslee@gmail.com>
2017-11-30 09:36:04 -0600

    change to test config

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,6 +1,7 @@
 console.log("CONFIGS")
 
 module.exports = {
+  "transform": {".*": "<rootDir>/node_modules/babel-jest"},
   snapshotSerializers: ["enzyme-to-json/serializer"],
   setupFiles: ["./jest.setup.js"]
 };
