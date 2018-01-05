6ee61f2f294de71360eb66fb1fe69a856521cbc9
Carl Peaslee <cpeaslee@gmail.com>
2018-01-04 19:16:04 -0600

    added a better startup script

--- scripts/reinitialize.js
+++ scripts/reinitialize.js
@@ -5,17 +5,40 @@ import {createAssociations} from '../db/associations'
 import rimraf from 'rimraf'
 import mkdirp from 'mkdirp'
 import path from 'path'
+import Organization from '../db/models/Organization'
+import User_Organization from '../db/models/User_Organization'
 
 async function reinitialize(){
   try {
 
     await db.query("SET foreign_key_checks = 0;")
 
+    await createAssociations()
+
     await db.sync({force: true})
 
     await db.query("SET foreign_key_checks = 1;")
 
-    await createAssociations()
+    let [organization] = await Organization.findOrCreate({
+      where: {
+        subdomain: 'local'
+      },
+      defaults: {
+        name: "Local",
+      }
+    })
+
+
+    await User_Organization.findOrCreate({
+      where: {
+        organizationId: organization.id,
+        userId: "localuser"
+      },
+      defaults: {
+        role: "admin"
+      }
+    })
+
 
     rimraf('localFileStorage', (err) => {
       mkdirp('localFileStorage', (err) => {
--- scripts/start.js
+++ scripts/start.js
@@ -1,9 +1,31 @@
 var program = require('commander')
 
 program
-  .arguments('<file>')
-  .option('-e, --env <env>', 'The current environment')
-  .option('-db, --database <database>', 'The current database')
+  .option('-e, --env <dev|production>', 'environment configuration', 'dev')
+  .option('-db, --database <local|dev>', 'database configuration', 'local')
+  .option('-fs, --filestorage <local|s3>', 'filestorage provider', 'local')
+  .option('-u, --authentication <local|auth0>', 'authentication strategy', 'local')
+  .option('-n, --next', 'start next')
+  .option('-a, --api', 'start api')
   .parse(process.argv)
 
-console.log(program.env)
+
+process.env.NODE_ENV = program.env
+
+process.env.DATABASE = program.database
+
+process.env.FILE_STORAGE = program.filestorage
+
+process.env.AUTH_STRATEGY = program.authentication
+
+if (program.env === 'dev') {
+
+  if (program.api){
+    process.env.BABEL_ENV = 'api'
+    require("babel-register")
+    var api = require('../api/index.js')
+  } else if (program.next){
+    process.env.BABEL_ENV = 'next'
+    var router = require('../router')
+  }
+}
