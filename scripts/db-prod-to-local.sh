3c0fdc0555f4d57d833960a0f61a3f0cd9157764
Carl Peaslee <cpeaslee@gmail.com>
2018-05-03 14:26:55 -0500

    some error with last deployment

--- scripts/db-prod-to-local.sh
+++ scripts/db-prod-to-local.sh
@@ -3,6 +3,8 @@
 
 source ../config/.env
 
-mysqldump -u $DB_PROD_USER -p$DB_PROD_PW -h $DB_PROD_HOST -P $DB_PROD_PORT $DB_PROD_NAME > prodDB.sql
+mysqldump -u $DB_PROD_USER -p$DB_PROD_PW -h $DB_PROD_HOST -P $DB_PROD_PORT --set-gtid-purged=OFF $DB_PROD_NAME > prodDB.sql
 
-mysql -u $DB_LOCAL_USER -p$DB_LOCAL_PW -h $DB_LOCAL_HOST -P $DB_LOCAL_PORT $DB_LOCAL_NAME < prodDB.sql
+mysql -u $DB_LOCAL_USER -h $DB_LOCAL_HOST -P $DB_LOCAL_PORT $DB_LOCAL_NAME < prodDB.sql
+
+rm -f prodDB.sql
