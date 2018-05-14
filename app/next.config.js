1afd66aa27ab6798ba169f863662556cb1bacfaa
Carl Peaslee <cpeaslee@gmail.com>
2018-05-14 13:14:04 -0500

    something cooky is happening with yarnlock file perhaps

--- app/next.config.js
+++ app/next.config.js
@@ -1,25 +1,23 @@
-const webpack = require('webpack')
+const webpack = require("webpack")
 
-if (process.env.NODE_ENV !== 'production'){
-  require('dotenv').config({
-    path: '../config/.env'
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({
+    path: "../config/.env"
   })
 }
-
 
 module.exports = {
   webpack: config => {
     config.plugins.push(
       new webpack.DefinePlugin({
-        'process.env.NOW_URL': JSON.stringify(process.env.NOW_URL),
-        'process.env.API_URL': JSON.stringify(process.env.API_URL),
-        'process.env.S3_URL': JSON.stringify(process.env.S3_URL),
-        'process.env.AUTH_STRATEGY': JSON.stringify(process.env.AUTH_STRATEGY),
-        'process.env.FILE_STORAGE': JSON.stringify(process.env.FILE_STORAGE),
-        'process.env.LOCAL_TILE_URL': JSON.stringify(process.env.LOCAL_TILE_URL)
-
+        "process.env.NOW_URL": JSON.stringify(process.env.NOW_URL),
+        "process.env.API_URL": JSON.stringify(process.env.API_URL),
+        "process.env.S3_URL": JSON.stringify(process.env.S3_URL),
+        "process.env.AUTH_STRATEGY": JSON.stringify(process.env.AUTH_STRATEGY),
+        "process.env.FILE_STORAGE": JSON.stringify(process.env.FILE_STORAGE),
+        "process.env.LOCAL_TILE_URL": JSON.stringify(process.env.LOCAL_TILE_URL)
       })
-    );
+    )
     return config
   }
 }
