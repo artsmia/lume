150afefb2beb1a2e3a9cad0971e2d82f5d184960
Carl Peaslee <cpeaslee@gmail.com>
2018-05-23 21:06:16 -0500

    changing commit to tag

--- config/set-env-vars.sh
+++ config/set-env-vars.sh
@@ -1,16 +1,23 @@
 #! /bin/bash
 
+echo $TRAVIS_COMMIT
+echo $TRAVIS_TAG
+
+
 if [$TRAVIS_BRANCH == 'production']
 then
-  echo "SUBDOMAIN=" >> ./config/.env.staging
+  echo "SUBDOMAIN=$TRAVIS_TAG" >> ./config/.env.staging
 elif [$TRAVIS_BRANCH == 'staging']
 then
   echo "SUBDOMAIN=staging." >> ./config/.env.staging
 else
-  echo "SUBDOMAIN=$TRAVIS_COMMIT." >> ./config/.env.staging
+  echo "SUBDOMAIN=$TRAVIS_TAG." >> ./config/.env.staging
 fi
 
 
 echo "LUME_URL=https://$SUBDOMAINlume.space" >> ./config/.env.staging
 echo "CMS_URL=https://$SUBDOMAINcms.lume.space" >> ./config/.env.staging
 echo "API_URL=https://$SUBDOMAINapi.lume.space" >> ./config/.env.staging
+
+
+export MY_TEST_VAR="hello there"
