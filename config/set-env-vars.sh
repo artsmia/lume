ad12fa3d06c30fd49dfd92fa86f7972763378211
Carl Peaslee <cpeaslee@gmail.com>
2018-05-23 20:48:43 -0500

    make deployments for every commit to master

---
+++ config/set-env-vars.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+if [$TRAVIS_BRANCH == 'production']
+then
+  echo "SUBDOMAIN=" >> ./config/.env.staging
+elif [$TRAVIS_BRANCH == 'staging']
+then
+  echo "SUBDOMAIN=staging." >> ./config/.env.staging
+else
+  echo "SUBDOMAIN=$TRAVIS_COMMIT." >> ./config/.env.staging
+fi
+
+
+echo "LUME_URL=https://$SUBDOMAINlume.space" >> ./config/.env.staging
+echo "CMS_URL=https://$SUBDOMAINcms.lume.space" >> ./config/.env.staging
+echo "API_URL=https://$SUBDOMAINapi.lume.space" >> ./config/.env.staging
