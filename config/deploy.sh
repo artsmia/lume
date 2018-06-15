ccaa6cd25fb30c842a60ea7c71e57dddc9d2d43a
Carl Peaslee <carl@Carls-MacBook-Pro.local>
2018-06-15 16:40:25 -0500

    trying to use the lume team

--- config/deploy.sh
+++ config/deploy.sh
@@ -25,16 +25,15 @@ CMS_URL=https://${SUBDOMAIN}cms.lume.space
 API_URL=https://${SUBDOMAIN}api.lume.space
 " >> ./config/.env
 
-now switch lume -t $NOW_TOKEN
 cd app
-now -t $NOW_TOKEN --dotenv=../config/.env
-now alias "${SUBDOMAIN}lume.space" -t $NOW_TOKEN
-now alias "${SUBDOMAIN}cms.lume.space" -t $NOW_TOKEN
+now -t $NOW_TOKEN --dotenv=../config/.env -T lume
+now alias "${SUBDOMAIN}lume.space" -t $NOW_TOKEN -T lume
+now alias "${SUBDOMAIN}cms.lume.space" -t $NOW_TOKEN -T lume
 cd ../data-api
 yarn install
 yarn run prep-build
-now -e NODE_ENV=production -t $NOW_TOKEN --dotenv=../config/.env
-now alias "${SUBDOMAIN}api.lume.space" -t $NOW_TOKEN
+now -e NODE_ENV=production -t $NOW_TOKEN --dotenv=../config/.env -T lume
+now alias "${SUBDOMAIN}api.lume.space" -t $NOW_TOKEN -T lume
 cd ..
 
 
@@ -51,11 +50,11 @@ if [ $TRAVIS_BRANCH != 'production' ]; then
   " >>  ./config/.env
 
   cd app
-  now -t $NOW_TOKEN --dotenv=../config/.env
-  now alias "${BRANCH}lume.space" -t $NOW_TOKEN
-  now alias "${BRANCH}cms.lume.space" -t $NOW_TOKEN
+  now -t $NOW_TOKEN --dotenv=../config/.env -T lume
+  now alias "${BRANCH}lume.space" -t $NOW_TOKEN -T lume
+  now alias "${BRANCH}cms.lume.space" -t $NOW_TOKEN -T lume
   cd ../data-api
-  now -e NODE_ENV=production -t $NOW_TOKEN --dotenv=../config/.env
-  now alias "${BRANCH}api.lume.space" -t $NOW_TOKEN
+  now -e NODE_ENV=production -t $NOW_TOKEN --dotenv=../config/.env -T lume
+  now alias "${BRANCH}api.lume.space" -t $NOW_TOKEN -T lume
   cd ..
 fi
