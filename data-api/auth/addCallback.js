53f77cc85bf9d15be86b953d03a6ad1f8bae16e5
Carl Peaslee <carl@Carls-MacBook-Pro.local>
2018-06-15 20:30:28 -0500

    manually whitelisting callbacks with auth0 mgmt api

---
+++ data-api/auth/addCallback.js
@@ -0,0 +1,23 @@
+import auth0 from './auth0'
+
+if (process.env.NODE_ENV === 'production'){
+  auth0.getClient({
+    client_id: process.env.AUTH0_CLIENT_ID
+  }, (err, client) => {
+    if (err) console.log(err)
+    console.log(client)
+    let {callbacks} = client
+
+    callbacks.push(`${process.env.CMS_URL}/callback`,`${process.env.LUME_URL}/callback` )
+
+    auth0.updateClient({
+      client_id: process.env.AUTH0_CLIENT_ID,
+    }, {
+      callbacks
+    }, (err, client) => {
+      if (err) console.log(err)
+      console.log(client)
+    })
+
+  })
+}
