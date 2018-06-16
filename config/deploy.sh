7a337bcf8161ed3dfff568c539c9a038099a9c75
Carl Peaslee <carl@Carls-MacBook-Pro.local>
2018-06-15 19:35:14 -0500

    making deploy script faster

--- config/deploy.sh
+++ config/deploy.sh
@@ -26,17 +26,40 @@ CMS_URL=https://${SUBDOMAIN}cms.lume.space
 API_URL=https://${SUBDOMAIN}api.lume.space
 " >> ./config/.env
 
-cd app
-now -t $NOW_TOKEN --dotenv=../config/.env -T lume
-now alias "${SUBDOMAIN}lume.space" -t $NOW_TOKEN -T lume
-now alias "${SUBDOMAIN}cms.lume.space" -t $NOW_TOKEN -T lume
 cd ../data-api
 yarn install
 yarn run prep-build
-now -e NODE_ENV=production -t $NOW_TOKEN --dotenv=../config/.env -T lume
-now alias "${SUBDOMAIN}api.lume.space" -t $NOW_TOKEN -T lume
 cd ..
 
+deployApp(){
+  cd app
+  now -t $NOW_TOKEN --dotenv=../config/.env -T lume
+  now alias "${SUBDOMAIN}lume.space" -t $NOW_TOKEN -T lume
+  now alias "${SUBDOMAIN}cms.lume.space" -t $NOW_TOKEN -T lume
+}
+
+deployApi(){
+  cd data-api
+  now -e NODE_ENV=production -t $NOW_TOKEN --dotenv=../config/.env -T lume
+  now alias "${SUBDOMAIN}api.lume.space" -t $NOW_TOKEN -T lume
+}
+
+# cd app
+# now -t $NOW_TOKEN --dotenv=../config/.env -T lume
+# now alias "${SUBDOMAIN}lume.space" -t $NOW_TOKEN -T lume
+# now alias "${SUBDOMAIN}cms.lume.space" -t $NOW_TOKEN -T lume
+# cd ../data-api
+# yarn install
+# yarn run prep-build
+# now -e NODE_ENV=production -t $NOW_TOKEN --dotenv=../config/.env -T lume
+# now alias "${SUBDOMAIN}api.lume.space" -t $NOW_TOKEN -T lume
+# cd ..
+
+
+deployApp &
+deployApi &
+
+wait
 
 if [ $TRAVIS_BRANCH != 'production' ]; then
 
