d867e0740e8191adad332666a41408ed73b14b94
Carl Peaslee <carl@Carls-MacBook-Pro.local>
2018-06-18 18:11:22 -0500

    forgot a period in dotenv string

--- config/deploy.sh
+++ config/deploy.sh
@@ -10,7 +10,7 @@ makeEnvVars(){
 deployApp(){
   makeEnvVars $1
   cd app
-  now -t $NOW_TOKEN --dotenv=../config/.env$2 -T lume -e LUME_URL -e CMS_URL -e API_URL
+  now -t $NOW_TOKEN --dotenv=../config/.env.$2 -T lume -e LUME_URL -e CMS_URL -e API_URL
   now alias "${1}lume.space" -t $NOW_TOKEN -T lume
   now alias "${1}cms.lume.space" -t $NOW_TOKEN -T lume
   echo "App is now deployed at ${1}lume.space and ${1}cms.lume.space"
@@ -20,7 +20,7 @@ deployApp(){
 deployApi(){
   makeEnvVars $1
   cd data-api
-  now -e NODE_ENV=production -t $NOW_TOKEN --dotenv=../config/.env$2 -T lume -e LUME_URL -e CMS_URL -e API_URL
+  now -e NODE_ENV=production -t $NOW_TOKEN --dotenv=../config/.env.$2 -T lume -e LUME_URL -e CMS_URL -e API_URL
   now alias "${1}api.lume.space" -t $NOW_TOKEN -T lume
   echo "Api is now deployed at ${1}api.lume.space"
 }
@@ -46,8 +46,8 @@ if [ $TRAVIS_BRANCH == "master" ]; then
 else
   TAG=$(echo $TRAVIS_COMMIT | cut -c1-7)
 
-  deployApp $TAG "staging" &
-  deployApi $TAG "staging" &
+  deployApp "$TAG." "staging" &
+  deployApi "$TAG." "staging" &
   deployApp $TRAVIS_BRANCH "staging" &
   deployApi $TRAVIS_BRANCH "staging" &
   wait
