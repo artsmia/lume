84fd44ea8e7a06e40ad3b02736de7626c973827a
Carl Peaslee <carl@Carls-MacBook-Pro.local>
2018-06-19 06:19:51 -0500

    trying to commit this cms branch

--- hooks/reservedBranches.sh
+++ hooks/reservedBranches.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 
 
-reserved=(cms api dev lume)
+reserved=("cms" "api" "dev" "lume")
 
 echo "
 Checking for reserved branches: (${reserved[*]})
 "
-echo ""
 
-branch=$(git symbolic-ref --short HEAD)
+branch="$(git symbolic-ref --short HEAD)"
 
 
 for name in "${reserved[*]}"
 do
+  echo "$name"
+  echo "$branch"
   if [ "$name" == "$branch" ]
   then
     echo "Please choose a new name for your branch. $branch is a a reserved branch name."
