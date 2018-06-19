8016bcfd8a0bed860e29e7ca5532d88a4dd65dc6
Carl Peaslee <carl@Carls-MacBook-Pro.local>
2018-06-19 05:55:03 -0500

    trying rev-parse

--- hooks/reservedBranches.sh
+++ hooks/reservedBranches.sh
@@ -2,6 +2,20 @@
 
 echo "Reserved Branches: cms, api, lume, dev"
 
+reserved=( 'cms' 'api' 'dev' 'lume' )
+
+echo $reserved
+
 branch=$(git symbolic-ref --short HEAD)
 
 echo $branch
+
+for name in $reserved
+do
+  echo $name
+  if [ $name == $branch ]
+  then
+    echo "Please choose a new name for your branch. $branch is a a reserved branch name."
+    exit 1
+  fi
+done
